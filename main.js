433d5200098a53969686851c3d85460e9a8251b1
Simon Walker <s.r.walker101@googlemail.com>
2017-04-18 09:51:40 +0100

    Set up basic javascript code

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var elem = document.getElementById("plotCanvas");
+    add_click_elements(elem, 'image.png', 'regions.json');
+}
+
+function add_click_elements(elem, image_url, regions_url) {
+    console.log('Adding click elements to ', elem);
+}
